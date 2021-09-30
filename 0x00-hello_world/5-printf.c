f3c1c75287692ccc3a59365c3c826dc271281784
axel valentin <axel.valentinbvs@yahoo.com>
2021-09-30 11:17:56 +0000

    update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 /**
  * main - main
@@ -7,6 +6,6 @@
  */
 int main(void)
 {
-printf("%s\n with proper grammar, but the outcome is a piece of art,");
+printf("%s\n", "with proper grammar, but the outcome is a piece of art,");
 return (0);
 }
