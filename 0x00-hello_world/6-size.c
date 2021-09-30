20b2eda30290d9dbbc833a224daa63e7a220050c
axel valentin <axel.valentinbvs@yahoo.com>
2021-09-30 11:36:31 +0000

    update

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ float floatType;
 double doubleType;
 char charType;
 
-printf("Size of int: %\n", sizeof(intType));
+printf("Size of int: %zu bytes\n", sizeof(intType));
 printf("Size of float: %zu bytes\n", sizeof(floatType));
 printf("Size of long int: %zu bytes\n", sizeof(longintType));
-printf("Size of char: %zu byte\n", sizeof(charType));
-printf("size of long long int: %zu byte\n", sizeof(lonlongintType));
+printf("Size of char: %zu bytes\n", sizeof(charType));
+printf("size of long long int: %zu bytes\n", sizeof(lonlongintType));
 return (0);
 }
