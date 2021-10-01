8c9d62738d2fe2d897d901b5712987fd6de34620
axel valentin <axel.valentinbvs@yahoo.com>
2021-10-01 13:02:55 +0000

    update

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Main
+ *
+ * Return: 0
+ */
+
+
+int main(void)
+{
+char n1 = 48;
+char n2 = 97;
+
+while (n1 <= 57)
+{
+putchar(n1);
+n1++;
+}
+while (n2 <= 102)
+{
+putchar(n2);
+n2++;
+}
+putchar ('\n');
+return (0);
+}
