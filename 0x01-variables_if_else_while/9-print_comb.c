6689092829df50a0b71776370dbd4453a566d8fc
axel valentin <axel.valentinbvs@yahoo.com>
2021-10-01 14:30:51 +0000

    uptade

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,22 +1,24 @@
 #include <stdio.h>
+
 /**
  * main - Main
  *
  * Return: 0
  */
+
 int main(void)
 {
-int n1 = 48;
-while (n1 <= 57)
-{  
-putchar(n1);
-n1++;
-if (n1 <= 57)
+int n = 48;
+while (n <= 57)
+{
+if (n > 48)
 {
 putchar(',');
 putchar(' ');
 }
- 
+putchar(n);
+n++;
 }
+putchar ('\n');
 return (0);
 }
