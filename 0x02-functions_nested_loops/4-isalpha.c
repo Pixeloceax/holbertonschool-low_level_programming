2aebb9f1e478eccad297eacf12c690a622851fcf
axel valentin <axel.valentinbvs@yahoo.com>
2021-10-05 13:07:39 +0000

    update

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * _isalpha - lower case and upper case send 1
  *
- * Return: Always 0.
+ * @c: variable
+ *
+ * Return: 1 is lower and upper case 0 to the other
  */
 
 int _isalpha(int c)
-{ 
-  if ((c > 65 && c > 90) || (c > 97 && c > 122))
-  {
-    return (1);
-  }
-  else
-    {
-      return (0);
-    }
+{
+	if ((c > 65 && c < 90) || (c > 97 && c < 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
