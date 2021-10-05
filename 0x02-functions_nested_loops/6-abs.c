6559e5438f4e45d62d9dae3aea1a30727bb906f1
axel valentin <axel.valentinbvs@yahoo.com>
2021-10-05 13:34:32 +0000

    update

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_sign - sign for number
+ *
+ * @n: variable
+ *
+ * Return: 1 is lower and upper case 0 to the other
+ */
+
+int _abs(int n )
+{
+  
+    if (n < 0)
+	{
+		n = n * -1;
+	}
+	return (n);
+}
