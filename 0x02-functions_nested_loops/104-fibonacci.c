4f40d1d9e5948bcaa446f2d2ad97ce001fd2a08e
axel valentin <axel.valentinbvs@yahoo.com>
2021-10-05 20:53:40 +0000

    update

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,32 +8,42 @@
 
 int main(void)
 {
-	long int a = 0;
-	long int b = 1;
-	long int c;
-	int i;
+	int count;
+	unsigned long a = 0, b = 1, i;
+	unsigned long m1a, m1b, m2a, m2b;
+	unsigned long ma, mb;
 
-	for (i = 2; i < 101; i++)
+	for (count = 0; count < 92; count++)
 	{
-		if (i <= 1)
+		i = a + b;
+		printf("%lu, ", i);
+
+		a = b;
+		b = i;
+	}
+	m1a = a / 10000000000;
+	m2a = b / 10000000000;
+	m1b = a % 10000000000;
+	m2b = b % 10000000000;
+	for (count = 93; count < 99; count++)
+	{
+		ma = m1a + m2a;
+		mb = m1b + m2b;
+		if (m1b + m2b > 9999999999)
 		{
-			c = i;
+			ma += 1;
+			mb %= 10000000000;
 		}
-		else
-		{
-			c = a + b;
-			a = b;
-			b = c;
-		}
-		printf("%lu", c);
-		if (i != 99)
+		printf("%lu%lu", ma, mb);
+		if (count != 98)
 		{
 			printf(", ");
 		}
-		if (i == 98)
-		{
-			printf("\n");
-		}
+		m1a = m2a;
+		m1b = m2b;
+		m2a = ma;
+		m2b = mb;
 	}
+	printf("\n");
 	return (0);
 }
