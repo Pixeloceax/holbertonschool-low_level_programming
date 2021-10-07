f6ac2f7e2bcd927bd0aba2bebcfb3882a0560159
axel valentin <axel.valentinbvs@yahoo.com>
2021-10-07 08:47:55 +0000

    upadte

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+    int a = 0;
+    for (a = 0; a <= n; a++)
+    {
+        _putchar(95);
+    }
+    _putchar('\n');
+
+}
