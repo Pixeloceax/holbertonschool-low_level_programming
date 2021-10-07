b6866a2812a73498d50bbfffd4d31a46e07c3e61
axel valentin <axel.valentinbvs@yahoo.com>
2021-10-07 14:44:48 +0000

    updaye

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * main - check the code.
- *
+ * _isupper - check the code.
+ *@c: comment
  * Return: Always 0.
  */
 int _isupper(int c)
 {
-	if ( c >= 'A' && c >= 'Z')
+	if (c >= 'A' && c >= 'Z')
 	{
 		return (1);
 	}
