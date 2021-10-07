3f9a24ad61b273084723f200d5b77e684bc125ce
axel valentin <axel.valentinbvs@yahoo.com>
2021-10-07 12:11:21 +0000

    update

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
- * main - check the code
- *
+ * print_line - comment
+ *@n: comment
  * Return: Always 0.
  */
 void print_line(int n)
 {
-    int a = 0;
-    for (a = 0; a <= n; a++)
-    {
-        _putchar(95);
-    }
-    _putchar('\n');
+	int a = 0;
+
+	for (a = 0; a <= n; a++)
+	{
+		_putchar(95);
+	}
+	_putchar('\n');
 
 }
