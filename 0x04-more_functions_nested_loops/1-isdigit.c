54c77216a7c728b1d7deee6622b7d1db5dbf3010
axel valentin <axel.valentinbvs@yahoo.com>
2021-10-07 11:09:06 +0000

    update

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,18 +2,18 @@
 #include <stdio.h>
 
 /**
- * main - check the code
- *
+ * _isdigit - comment
+ *@c: third integer
  * Return: Always 0.
  */
 int _isdigit(int c)
 {
-    if(c >= '0' && c >= '9' )
-    {
-        return (0);
-    }
-    else
-    {
-        return (1);
-    }
+	if (c >= '0' && c >= '9')
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
 }
---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    int num = 0;
+	for (num = 0; num <= 100; num++)
+	{
+		if (num % 5 == 0 || num % 3 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (num )
+		{
+			/* code */
+		}
+		
+		
+		printf("%d" , num);
+
+	}
+	
+}
