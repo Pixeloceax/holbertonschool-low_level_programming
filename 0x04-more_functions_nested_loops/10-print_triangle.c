af3753e34440356fff4c6a00610cee7fd7433147
axel valentin <axel.valentinbvs@yahoo.com>
2021-10-07 13:14:53 +0000

    update

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,38 @@
+
+#include "main.h"
+
+/**
+ * print_triangle - print triangle
+ * @size: int
+ * Return: multipe
+ */
+
+void print_triangle(int size)
+{
+	int a = 1;
+	int b;
+
+	if (size != 0)
+	{
+		while (a <= size)
+		{
+			b = 1;
+			while (b <= size - a)
+			{
+				_putchar(' ');
+				b++;
+			}
+			while (b <= size)
+			{
+				_putchar('#');
+				b++;
+			}
+			_putchar('\n');
+			a++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
