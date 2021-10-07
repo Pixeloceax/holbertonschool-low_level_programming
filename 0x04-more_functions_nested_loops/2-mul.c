884207485e4282c94d6fc347abb7c794324f9bde
axel valentin <axel.valentinbvs@yahoo.com>
2021-10-07 11:37:37 +0000

    update

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -1,14 +1,12 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
- *
+ * mul - comment
+ * @a: comment
+ * @b: comment
  * Return: Always 0.
  */
 int mul(int a, int b)
 {
 	return (a * b);
 }
-
-  
