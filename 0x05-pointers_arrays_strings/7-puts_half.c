b8cfd395a99dda50aa6981e54129629c437f27ed
axel valentin <axel.valentinbvs@yahoo.com>
2021-10-11 13:20:00 +0000

    update

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * puts_half - check the code
+ *@str : oui
+ * Return: Always 0.
+ */
+void puts_half(char *str)
+{
+	int max, divmax;
+
+	for (max = 0; str[max] != '\0'; max++)
+	{
+		continue;
+	}
+	if (max % 2 != 0)
+	{
+		divmax = (max + 1) / 2;
+	}
+	else
+	{
+		divmax = max / 2;
+	}
+	while (str[divmax] != '\0')
+	{
+		_putchar(str[divmax]);
+		divmax++;
+	}
+	_putchar('\n');
+}
