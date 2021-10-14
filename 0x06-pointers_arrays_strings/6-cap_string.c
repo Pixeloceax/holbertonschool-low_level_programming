89d66a44c5aff76c555181eca6ada88732a347f8
axel valentin <axel.valentinbvs@yahoo.com>
2021-10-14 11:56:49 +0000

    update

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * cap_string - check the code
+ *@s : mdqfj
+ * Return: Always 0.
+ */
+char *cap_string(char *s)
+{
+	int a, b;
+	char c[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+
+	a = 1;
+	if (s[0] >= 97 && s[0] <= 122)
+	{
+		s[0] = s[0] - 32;
+	}
+	while (s[a] != '\0')
+	{
+		for (b = 0; c[b] != '\0'; b++)
+		{
+			if (s[a - 1] == c[b] && (s[a] >= 97 && s[a] <= 122))
+			{
+				s[a] = s[a] - 32;
+			}
+		}
+		a++;
+	}
+	return (s);
+}
