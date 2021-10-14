a65b33820fa3d6d91732d35bd6ba70bc1073c6bf
axel valentin <axel.valentinbvs@yahoo.com>
2021-10-14 13:49:33 +0000

    update

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_number(int n)
+{
+	unsigned int a;
+
+	if (n < 0)
+	{
+		a = -n;
+		_putchar(45);
+	}
+	else
+	{
+		a = n;
+	}
+	if (a / 10)
+	{
+		print_number(a / 10);
+	}
+	_putchar((a % 10) + 48);
+} 
