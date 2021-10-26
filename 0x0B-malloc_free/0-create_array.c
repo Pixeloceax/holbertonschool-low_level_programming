7764fa429e1498e641eb0bfdb88cf3fcdb457d38
axel valentin <axel.valentinbvs@yahoo.com>
2021-10-26 08:25:11 +0000

    first malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,10 +2,9 @@
 #include <stdlib.h>
 
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
+ *  create_array - prints buffer in hexa
+ * @c : c
+ * @size : c
  * Return: Nothing.
  */
 char *create_array(unsigned int size, char c)
