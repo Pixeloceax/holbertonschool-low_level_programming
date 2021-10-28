9e9c80d41a9949db504f7f6a8ed4a668a4b91622
axel valentin <axel.valentinbvs@yahoo.com>
2021-10-28 10:59:04 +0000

    3 check

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,15 +1,28 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
- * _calloc - man calloc
- * @nmemb: nmemb
- * @size: size
+ * _calloc - Recode the calloc function
  *
- * Return: NULL.
+ * @nmemb: Length of the array that contain pointers
+ * @size: Size of the elements pointed
+ *
+ * Return: NULL if any problem, pointer to the array else (succes)
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	
+	char *pointer;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	pointer = malloc(nmemb * size);
+	if (pointer == NULL)
+		return (NULL);
+
+	for (i = 0; i < size * nmemb; i++)
+		*(pointer + i) = 0;
+
+	return (pointer);
 }
