1d9e1e701375231eb5998274d3eea19ff3d06c9d
axel valentin <axel.valentinbvs@yahoo.com>
2021-10-28 11:31:45 +0000

    4 check

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -8,55 +8,38 @@ int _strlen(char *s);
  *
  * @s1: Str one
  * @s2: Str two
- * @n : nombers of charat
+ * @n: Size that we want to concatenate
  *
  * Return: NULL if probleme, pointer to new str else (succes)
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-
-	char *sFinal = NULL;
-	unsigned int length1, length2, totalLenght, i;
+	char *sFinal;
+	unsigned int length1, length2, i;
 
 	if (s1 == NULL)
 		length1 = 0;
 	else
-		length1 = _strlen(s1);
-
+	{
+		for (length1 = 0; *(s1 + length1); length1++)
+			;
+	}
 	if (s2 == NULL)
 		length2 = 0;
 	else
-		length2 = _strlen(s2);
-
-	totalLenght = length2 + length1;
-	sFinal = (char *)malloc(sizeof(char) * totalLenght + 5);
-
+	{
+		for (length2 = 0; *(s2 + length2); length2++)
+			;
+	}
+	if (n < length2)
+		length2 = n;
+	sFinal = malloc(sizeof(char) * (length1 + length2 + 6));
 	if (sFinal == NULL)
 		return (NULL);
-
 	for (i = 0; i < length1; i++)
-		*(sFinal + i) = *(s1 + i);
-	for (i = 0; i < n; i++)
-		*(sFinal + length1 + i) = *(s2 + i);
+		sFinal[i] = s1[i];
+	for (i = 0; i < length2; i++)
+		sFinal[length1 + i] = s2[i];
 	*(sFinal + length1 + length2) = '\0';
 	return (sFinal);
-
-}
-
-/**
- * _strlen - Calculate the length of a string.
- *
- * @s: String to manipulate.
- *
- * Return: The length of the inputed string.
- */
-
-int _strlen(char *s)
-{
-	int i;
-
-	for (i = 0; s[i] != '\0'; i++)
-	{
-	}
-	return (i);
 }
