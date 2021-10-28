6ad7709531fcf53a6e2dfe238b67547ee1f3a1e2
axel valentin <axel.valentinbvs@yahoo.com>
2021-10-28 11:57:27 +0000

    malloc mandatory

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -2,14 +2,29 @@
 #include <stdlib.h>
 
 /**
- * _calloc - main fonction
- *
- * @nmemb: nmemb
- * @size: size
- *
- * Return: NULL 
+ * array_range - main fonction
+ * @min: int
+ * @max: int
+ * Return: NULL
  */
+
 int *array_range(int min, int max)
 {
-	
+	int i;
+	int size_A = max - min + 1;
+	int *p;
+
+	if (min > max)
+		return (NULL);
+
+	p = malloc(sizeof(int) * size_A);
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < size_A; i++)
+	{
+		p[i] = min;
+		min++;
+	}
+	return (p);
 }
