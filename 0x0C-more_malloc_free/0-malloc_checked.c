9e8866bd6bf031d6d3b8cbe45b9245d27d443c84
axel valentin <axel.valentinbvs@yahoo.com>
2021-10-28 10:39:05 +0000

    first check

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked  - main fonction 
+ *@b : variable
+ * Return: NULL
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *p = NULL;
+	p = malloc (b);
+
+	if (p == NULL)
+	{
+		free(p);
+		exit(98);
+	}
+	return (p);
+}
---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - man calloc
+ * @nmemb: nmemb
+ * @size: size
+ *
+ * Return: NULL.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	
+}
