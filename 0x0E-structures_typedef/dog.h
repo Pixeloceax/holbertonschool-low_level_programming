f364ccd7f1f76521baa5b4be4662d75f081bb477
axel valentin <axel.valentinbvs@yahoo.com>
2021-10-31 17:12:22 +0000

    betty style

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * struct dog - struct
+ *@name : name dog
+ *@owner : owner dog
+ *@age : age dog
+ */
+
 struct dog
 {
 	char *name;
 	char *owner;
 	float age;
 };
+#endif /* MAIN_H */
