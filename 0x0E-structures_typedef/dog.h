7bf9532c49b152b2acdec8657da32368d78b43a6
axel valentin <axel.valentinbvs@yahoo.com>
2021-11-01 09:43:59 +0000

    betty update

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -12,5 +12,8 @@ struct dog
 	char *name;
 	char *owner;
 	float age;
-};
+} dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif /* MAIN_H */
