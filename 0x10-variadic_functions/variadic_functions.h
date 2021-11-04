c74911efd714eb736bef0473980150cad3511487
axel valentin <axel.valentinbvs@yahoo.com>
2021-11-04 12:21:08 +0000

    betty update

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,3 +1,9 @@
+#ifndef function_variadic
+#define function_variadic
+
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
+
+#endif /* function_variadic */
