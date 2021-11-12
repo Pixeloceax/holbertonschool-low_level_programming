6b720ab420f29a4363daf18efa42d1fc994f79a5
axel valentin <axel.valentinbvs@yahoo.com>
2021-11-12 13:22:34 +0000

    add task 1

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ *list_len - function
+ *@h: h 
+ *Return: size_t 
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t counter = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			counter++;
+			h = h->next;
+		}
+	}
+	return (counter);
+}
