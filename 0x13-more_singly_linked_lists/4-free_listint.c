78fe70155834e303786f1f2174d41b912ab41caa
axel valentin <axel.valentinbvs@yahoo.com>
2021-11-16 12:23:12 +0000

    add

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ *free_listint - function
+ *@head: head
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
