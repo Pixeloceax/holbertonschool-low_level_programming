1f228deb6e2efdae7db3c12b8f65bac163856122
axel valentin <axel.valentinbvs@yahoo.com>
2021-11-16 11:24:01 +0000

    add betty style

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -1,5 +1,10 @@
 #include "lists.h"
-
+/**
+ *add_nodeint - function
+ *@head: head
+ *@n: n
+ *Return: listint_t*
+ */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new_node;
