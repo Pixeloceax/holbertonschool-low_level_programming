9b44dc1ecba5e3e5bb531a49df080174ea0e37e2
axel valentin <axel.valentinbvs@yahoo.com>
2021-11-18 13:43:54 +0000

    add task

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,11 @@
+#ifndef bit_manipulation
+#define bit_manipulation
+
+#include <stdio.h>
+#include <math.h>
+
+int _putchar(char c);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
+
+#endif /* bit_manipulation */
