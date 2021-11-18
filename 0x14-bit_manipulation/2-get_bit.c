326d7b661b23858952278889fc4f53addd11052f
axel valentin <axel.valentinbvs@yahoo.com>
2021-11-18 15:16:10 +0000

    add task

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,12 +1,26 @@
 #include "main.h"
+/**
+ *get_bit- Get the bit object
+ *@n: n
+ *@index: index
+ *Return: int
+ */
 int get_bit(unsigned long int n, unsigned int index)
 {
 	int i;
 
-	if (n == NULL)
+	if (n == 0)
 	{
 		return (0);
 	}
-	i = (n >> index) & 1;
+
+
+	if (index < 32)
+	{
+		i = (n >> index) & 1;
 	return (i);
+	}
+	else
+		return (-1);
+
 }
