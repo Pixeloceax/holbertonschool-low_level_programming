18b8850d9846a35a0ef64d09476c44252e505276
axel valentin <axel.valentinbvs@yahoo.com>
2021-11-22 12:35:16 +0000

    add task

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,21 +9,22 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE *fp = fopen(filename, "r");
-	char c;
+	ssize_t fd = open(filename, O_RDONLY);
+	char *c;
+	ssize_t size;
 
-	if (fp == NULL)
+	if (fd == -1)
 	{
 		return (0);
 	}
-
-	while ((c = fgetc(fp)) != EOF)
+	c = malloc(sizeof(char) * letters);
+	if (c == NULL)
 	{
-		putchar(c);
-		letters++;
+		return (0);
 	}
-	return (letters);
-	fclose(fp);
-
-	return (0);
+	
+	size = read(fd, c, letters);
+	write(1, c, size);
+	close(fd);
+	return (size);
 }
