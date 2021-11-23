682e6ca574edcb4fab0ba4b3118439679edc3085
axel valentin <axel.valentinbvs@yahoo.com>
2021-11-23 15:35:36 +0000

    update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,24 +1,68 @@
 #include "main.h"
+#include <stdio.h>
 
-int main(char *argv[], int argc)
+/**
+ *copy - function
+ *@src: src
+ *@dest: dest
+ */
+
+void copy(const char *src, const char *dest)
 {
-	int copy, read, write, end_copy;
-	char *buf;
+	int fd, fd2, _read;
+	char buff[1024];
 
+	fd = open(src, O_RDONLY);
+	if (!src || fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
+		exit(98);
+	}
+
+	fd2 = open(dest, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((_read = read(fd, buff, 1024)) > 0)
+	{
+		if (write(fd2, buff, _read) != _read || fd2 == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", dest);
+			exit(99);
+		}
+	}
+
+	if (_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", src);
+		exit(98);
+	}
+
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+
+	if (close(fd2) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd2);
+		exit(100);
+	}
+}
+
+/**
+ *main - function
+ *@argc: argc
+ *@argv: argv
+ *Return: int
+ */
+
+int main(int argc, char **argv)
+{
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, " Usage: ");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	buf = malloc(sizeof(char) * 1024);
-	if (buf == NULL)
-	{
-		dprintf(STDERR_FILENO, " Error ");
-		exit(99);
-	}
-	copy = open;
-	read = open;
-	end_copy = open;
-	
+	copy(argv[1], argv[2]);
+	exit(0);
 }
