2ab1957a5d45e0fc9305a79615c5ebf17e13bda4
axel valentin <axel.valentinbvs@yahoo.com>
2022-01-06 11:31:33 +0000

    add task

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,5 +1,22 @@
 #include "lists.h"
+/**
+ *get_dnodeint_at_index - Get the dnodeint at index object
+ *@head: V
+ *@index: V
+ *Return: dlistint_t*
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-	
+	unsigned int count = 0;
+
+	while (head != NULL)
+	{
+		if (count == index)
+		{
+			return (head);
+		}
+		count++;
+		head = head->next;
+	}
+	assert(0);
 }
