5f6144fb4d75d0cac612663a6393f34f72c6123e
axel valentin <axel.valentinbvs@yahoo.com>
2022-01-28 09:54:26 +0000

    add task

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -6,10 +6,11 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	hash_node_t *node;
-	unsigned int index, index_2;
-	printf("{");
+	unsigned int index, index_2 = 0;
+
 	if (ht != NULL)
 	{
+		printf("{");
 		for (index = 0; index < ht->size; index++)
 		{
 			node = ht->array[index];
@@ -17,13 +18,14 @@ void hash_table_print(const hash_table_t *ht)
 			{
 				if (index_2 != 0)
 				{
-					printf(", ");
-					index_2 = 1;
+					printf(", ");	
 				}
 				printf("'%s': '%s'", node->key, node->value);
+				index_2 += 1;
 				node = node->next;
+				
 			}
 		}
+		printf("}\n");
 	}
-	printf("}\n");
 }
