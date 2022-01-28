f6ab3c2ac18deb58ae7c282037afb6057fe3ba85
axel valentin <axel.valentinbvs@yahoo.com>
2022-01-28 08:45:47 +0000

    no size vérif + calloc

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,7 +6,7 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash = malloc(sizeof(hash_table_t) * size);
+	hash_table_t *hash = malloc(sizeof(hash_table_t));
 
 	if (hash == NULL)
 	{
